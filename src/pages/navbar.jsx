2185693fd146fe05f4c6b88b05faebfca6607fb9
Prashanth2025 <prashanthramesh.02@gmail.com>
2025-11-16 18:06:27 +0530

    all files

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -1,40 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./navbar.css";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+  const closeMenu = () => setIsOpen(false);
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm sticky-top">
       <div className="container-fluid px-4">
         {/* Brand */}
-        <NavLink className="navbar-brand fw-bold fs-4" to="/">
+        <NavLink
+          className="navbar-brand fw-bold fs-4"
+          to="/"
+          onClick={closeMenu}
+        >
           Prashanth<span className="text-primary">.Dev</span>
         </NavLink>
 
-        {/* Mobile Toggle */}
+        {/* Custom Toggle Button */}
         <button
           className="navbar-toggler"
           type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
+          onClick={toggleMenu}
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
 
         {/* Nav Links */}
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className={`collapse navbar-collapse ${isOpen ? "show" : ""}`}>
           <ul className="navbar-nav ms-auto d-flex align-items-center gap-1">
             <li className="nav-item">
-              <NavLink className="nav-link" to="/about">About</NavLink>
+              <NavLink className="nav-link" to="/about" onClick={closeMenu}>
+                About
+              </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/projects">Projects</NavLink>
+              <NavLink className="nav-link" to="/projects" onClick={closeMenu}>
+                Projects
+              </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/contact">Contact</NavLink>
+              <NavLink className="nav-link" to="/contact" onClick={closeMenu}>
+                Contact
+              </NavLink>
             </li>
           </ul>
         </div>
